530a7c4359a77a3d347c96d801355eb8d4c762f4
matiaspoullain <matias.poullain>
2021-03-22 21:40:37 -0300

    cambios chicos

--- Concurrencias_CABA/Descarga concurrencias parques.R
+++ Concurrencias_CABA/Descarga concurrencias parques.R
@@ -7,7 +7,7 @@ parques <- paste(parques$Nombre.de.la.plaza.o.plazoleta, ", Ciudad de Buenos Air
 library(sgat)
 initialization_sgat()
 
-
+parques <- salieron.mal
 va.por <- parques[1]
 salieron.mal <- c()
 repeticiones <- 0
@@ -17,7 +17,7 @@ for(resta in parques[which(parques == va.por):length(parques)]){
   concurrencia.parques.caba <- 1
   class(concurrencia.parques.caba) <- "try-error"
   intento <- 1
-  while(class(concurrencia.parques.caba) == "try-error" & intento <= 10){
+  while(class(concurrencia.parques.caba) == "try-error" & intento <= 20){
     concurrencia.parques.caba <- try(sgat(resta, carpeta.guardado = "CSVs Concurrencias/Parques CABA", tiempo.espera = 15), silent = TRUE)
     intento <- intento + 1
     repeticiones <- repeticiones + 1
