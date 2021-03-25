4dd3204ccb069747de09b8b6e00ecb6d4f5909c8
matiaspoullain <58349277+matiaspoullain@users.noreply.github.com>
2021-03-25 18:21:09 -0300

    Codigo para bares

--- Concurrencias_CABA/Descarga concurrencias bares.R
+++ Concurrencias_CABA/Descarga concurrencias bares.R
@@ -1,46 +1,47 @@
-#Descarga concurrencias parques CABA:
+#Descarga concurrencias bares CABA:
 library(git2r)
 bares <- read.csv("Concurrencias_CABA/Nombres bares CABA.csv")
 
 bares <- paste(bares$caba, ", Ciudad de Buenos Aires", sep = "")
 
+#salieron.mal <- read.csv("Concurrencias_CABA/bares salieron mal.csv")
+#salieron.mal <- salieron.mal$salieron.mal
 library(sgat)
 initialization_sgat()
 
-
-va.por <- bares[1]
+#bares <- salieron.mal
+ya.hizo <- c()
 salieron.mal <- c()
 repeticiones <- 0
 
-for(resta in bares[which(bares == va.por):length(bares)]){
-  print(paste("Va por ", which(bares == va.por), " de ", length(bares), ": ", 100* which(bares == va.por)/length(bares), " %", sep = "" ))
+for(resta in bares){
+  print(paste("Va por ", which(bares == resta), " de ", length(bares), ": ", 100* which(bares == resta)/length(bares), " %", sep = "" ))
+  ya.hizo <- c(ya.hizo, resta)
+  write.csv(ya.hizo, "Concurrencias_CABA/Bares que ya inicio.csv")
   concurrencia.bares.caba <- 1
   class(concurrencia.bares.caba) <- "try-error"
   intento <- 1
-  while(class(concurrencia.bares.caba) == "try-error" & intento <= 10){
-    concurrencia.bares.caba <- try(sgat(resta, carpeta.guardado = "CSVs Concurrencias/bares CABA", tiempo.espera = 15), silent = TRUE)
+  while(class(concurrencia.bares.caba) == "try-error" & intento <= 5){
+    concurrencia.bares.caba <- try(sgat(resta, carpeta.guardado = "CSVs Concurrencias/bares CABA nuevo sgat", tiempo.espera = 15), silent = TRUE)
     intento <- intento + 1
     repeticiones <- repeticiones + 1
   }
   if(class(concurrencia.bares.caba) == "try-error"){
     salieron.mal <- c(salieron.mal, resta)
+    write.csv(df.salieron.mal, "Concurrencias_CABA/bares salieron mal.csv")
   }
   if(repeticiones >= 100){
     Sys.sleep(300)
     repeticiones <- 0
   }
-  va.por <- resta
-
 }
 
 num <- 1:length(salieron.mal)
 df.salieron.mal <- data.frame(num, salieron.mal)
-write.csv(df.salieron.mal, "Concurrencias_CABA/bares salieron mal.csv")
-
 
 git2r::config(user.name = "matiaspoullain", user.email = "matias.poullain")
 git2r::status()
 git2r::add(repo = ".", path = getwd(), force = FALSE)
-git2r::commit(repo = ".", message = "subida datos bares", all = TRUE)
+git2r::commit(repo = ".", message = "datos bares", all = TRUE)
 shell("git push")
 
